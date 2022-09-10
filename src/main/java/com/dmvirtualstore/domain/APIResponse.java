ada28fc694492bca000518acdd50ed5ee1d16e1e
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-10 06:41:24 -0300

    Teste endpoint Categoria com result

---
+++ src/main/java/com/dmvirtualstore/domain/APIResponse.java
@@ -0,0 +1,25 @@
+package com.dmvirtualstore.domain;
+
+public class APIResponse {
+	
+	private Object result;
+
+	
+	public APIResponse(Object result) {
+		this.result = result;
+	}
+	
+
+	public Object getResult() {
+		return result;
+	}
+
+	public void setResult(Object result) {
+		this.result = result;
+	}
+	
+	
+	
+	
+
+}
--- src/main/java/com/dmvirtualstore/resources/CategoriaResource.java
+++ src/main/java/com/dmvirtualstore/resources/CategoriaResource.java
@@ -1,17 +1,34 @@
 package com.dmvirtualstore.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dmvirtualstore.domain.APIResponse;
+import com.dmvirtualstore.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public APIResponse listar() {
 		
-		return "Rest está funcionando!";
+		Categoria cat1 = new Categoria(1, "Brincos");
+		Categoria cat2 = new Categoria(1, "Colar");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		APIResponse result = new APIResponse(lista);
+		
+		return result;
 	}
 
 }
