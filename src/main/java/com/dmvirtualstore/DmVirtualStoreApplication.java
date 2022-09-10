af488340812b22025a2613e807c73f9b620eae48
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-10 05:50:41 -0300

    Projeto criado

---
+++ src/main/java/com/dmvirtualstore/DmVirtualStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dmvirtualstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmVirtualStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmVirtualStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmvirtualstore/DmVirtualStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmvirtualstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmVirtualStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
