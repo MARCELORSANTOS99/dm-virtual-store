02735737fa5f6369589f4db99fb743639a7a3075
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-11 06:30:27 -0300

    Operação de instanciação

--- src/main/java/com/dmvirtualstore/DmVirtualStoreApplication.java
+++ src/main/java/com/dmvirtualstore/DmVirtualStoreApplication.java
@@ -1,13 +1,35 @@
 package com.dmvirtualstore;
+import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DmVirtualStoreApplication {
+import com.dmvirtualstore.domain.Categoria;
+import com.dmvirtualstore.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DmVirtualStoreApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DmVirtualStoreApplication.class, args);
+	}
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Ouro");
+		Categoria cat2 = new Categoria(null, "Prata");
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+
 	}
 
 }
