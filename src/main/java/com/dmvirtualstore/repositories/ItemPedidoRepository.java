cd0215fa0da94bc60f1c5b6f49a9b5e9b79e4c69
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-12 06:32:47 -0300

    Inserindo uma nova Categoria com post

--- src/main/java/com/dmvirtualstore/repositories/ItemPedidoRepository.java
+++ src/main/java/com/dmvirtualstore/repositories/ItemPedidoRepository.java
@@ -2,7 +2,6 @@ package com.dmvirtualstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dmvirtualstore.domain.Cliente;
 import com.dmvirtualstore.domain.ItemPedido;
 
 @Repository
