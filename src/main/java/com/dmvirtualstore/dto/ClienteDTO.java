af8a2fbfb72b1b3ea16a0130dd1cda171937e78d
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-14 08:21:04 -0300

    Validação de emmail repetido na atualização

--- src/main/java/com/dmvirtualstore/dto/ClienteDTO.java
+++ src/main/java/com/dmvirtualstore/dto/ClienteDTO.java
@@ -8,8 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.dmvirtualstore.domain.Cliente;
+import com.dmvirtualstore.services.validation.ClienteUpdate;
 
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
