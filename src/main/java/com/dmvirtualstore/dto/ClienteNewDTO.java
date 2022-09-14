2e44eaffc96977a50f4bae3813ef5006411017f1
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-14 07:36:07 -0300

    Validação de CPF e CNPJ

--- src/main/java/com/dmvirtualstore/dto/ClienteNewDTO.java
+++ src/main/java/com/dmvirtualstore/dto/ClienteNewDTO.java
@@ -2,21 +2,46 @@ package com.dmvirtualstore.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.dmvirtualstore.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 
 
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message = "Prenchimento obrigatório")
+	@Length(min = 5, max=120, message = "O Tamanho deve ser entre 5 e 120 caracateres")
 	private String nome;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
+	@Email(message = "O Tamanho deve ser entre 5 e 120 caracateres")
 	private String email;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String cep;
 
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
