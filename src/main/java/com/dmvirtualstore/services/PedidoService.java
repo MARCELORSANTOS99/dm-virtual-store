b674e27794b757ecc423a0996f4f745060248502
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-19 18:21:05 -0300

    Cliente só recupera seus pedidos

--- src/main/java/com/dmvirtualstore/services/PedidoService.java
+++ src/main/java/com/dmvirtualstore/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.dmvirtualstore.domain.Cliente;
 import com.dmvirtualstore.domain.ItemPedido;
 import com.dmvirtualstore.domain.PagamentoComBoleto;
 import com.dmvirtualstore.domain.Pedido;
@@ -13,6 +17,8 @@ import com.dmvirtualstore.domain.enuns.EstadoPagamento;
 import com.dmvirtualstore.repositories.ItemPedidoRepository;
 import com.dmvirtualstore.repositories.PagamentoRepository;
 import com.dmvirtualstore.repositories.PedidoRepository;
+import com.dmvirtualstore.security.UserSS;
+import com.dmvirtualstore.services.exception.AuthorizationException;
 import com.dmvirtualstore.services.exception.ObjectNotFoundException;
 
 @Service
@@ -69,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
