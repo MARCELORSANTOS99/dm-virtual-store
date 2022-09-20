ec0beedb2bcd9cfb1699d715351d41715b97bc79
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-09-20 05:55:27 -0300

    Primeiro arquivo no S3

--- src/main/java/com/dmvirtualstore/DmVirtualStoreApplication.java
+++ src/main/java/com/dmvirtualstore/DmVirtualStoreApplication.java
@@ -1,39 +1,19 @@
 package com.dmvirtualstore;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dmvirtualstore.domain.Categoria;
-import com.dmvirtualstore.domain.Cidade;
-import com.dmvirtualstore.domain.Cliente;
-import com.dmvirtualstore.domain.Endereco;
-import com.dmvirtualstore.domain.Estado;
-import com.dmvirtualstore.domain.ItemPedido;
-import com.dmvirtualstore.domain.Pagamento;
-import com.dmvirtualstore.domain.PagamentoComBoleto;
-import com.dmvirtualstore.domain.PagamentoComCartao;
-import com.dmvirtualstore.domain.Pedido;
-import com.dmvirtualstore.domain.Produto;
-import com.dmvirtualstore.domain.enuns.EstadoPagamento;
-import com.dmvirtualstore.domain.enuns.TipoCliente;
-import com.dmvirtualstore.repositories.CategoriaRepository;
-import com.dmvirtualstore.repositories.CidadeRepository;
-import com.dmvirtualstore.repositories.ClienteRepository;
-import com.dmvirtualstore.repositories.EnderecoRepository;
-import com.dmvirtualstore.repositories.EstadoRepository;
-import com.dmvirtualstore.repositories.ItemPedidoRepository;
-import com.dmvirtualstore.repositories.PagamentoRepository;
-import com.dmvirtualstore.repositories.PedidoRepository;
-import com.dmvirtualstore.repositories.ProdutoRepository;
+import com.dmvirtualstore.services.S3Service;
 
 @SpringBootApplication
 public class DmVirtualStoreApplication implements CommandLineRunner {
 
-	
+	/*
+	@Autowired
+	private S3Service s3Service;
+	*/
+
 
 	
 	public static void main(String[] args) {
@@ -44,7 +24,8 @@ public class DmVirtualStoreApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		
+		//s3Service.uploadFile("E:\\Pictures\\amor\\20200216_130143.jpg");
+
 
 
 	}
